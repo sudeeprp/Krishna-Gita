1f4810a02c18692ad5547106c3314badd8429297
drishti1607 <48019207+drishti1607@users.noreply.github.com>
2020-05-10 11:51:19 +0530

    Separated .js into logic and DOM

--- VerseFeed/verseFeed.js
+++ VerseFeed/verseFeed.js
@@ -5,13 +5,6 @@ function getRandomInt(min, max) {
 }
 
 function randomFeed(numOfVerses, numOfVersesDisplayed) {
-    var verse = ["1 (1-1 to 1-11): We often doubt and worry about achieving wealth, success and other outcomes.",
-    "2 (1-12 to 1-19): In our anxiety, we interpret anything that happens as a signal of doom",
-    "3 (1-20 to 1-23): It doesn’t matter who you are or what you’re asking for. Be in this conversation. Keep enquiring.",
-    "4 (1-26 to 1-47): In the midst of action, we sometimes worry Am I going to hurt someone? Is the outcome worth it?",
-    "5 (2-11): Hesitation happens when you mix-up the Self with your body and the rest of the environment.",
-    "6 (2-12): The Self is separate from the environment and your body. The Self is ever-lasting. So is the Lord.",
-    "7 (2-12): Is there anything other than the observable universe"];
     verseNum = []
     var i = 1;
     var x = getRandomInt(1,numOfVerses);
@@ -19,11 +12,9 @@ function randomFeed(numOfVerses, numOfVersesDisplayed) {
         while(verseNum.includes(x)) {
             x = getRandomInt(1,7);
         }
-        var varVerse = document.getElementById("verseFeed" + i);
-        varVerse.innerHTML = verse[x-1];
         verseNum.push(x);
-        x = getRandomInt(1,7);
-        console.log(x);
+        x = getRandomInt(1,numOfVerses);
         i++;
     }
+    return verseNum;
 }
---
+++ VerseFeed/verseFeedDom.js
@@ -0,0 +1,16 @@
+function randomFeedDom(numOfVerses, numOfVersesDisplayed) {
+    var verse = ["1 (1-1 to 1-11): We often doubt and worry about achieving wealth, success and other outcomes.",
+    "2 (1-12 to 1-19): In our anxiety, we interpret anything that happens as a signal of doom",
+    "3 (1-20 to 1-23): It doesn’t matter who you are or what you’re asking for. Be in this conversation. Keep enquiring.",
+    "4 (1-26 to 1-47): In the midst of action, we sometimes worry Am I going to hurt someone? Is the outcome worth it?",
+    "5 (2-11): Hesitation happens when you mix-up the Self with your body and the rest of the environment.",
+    "6 (2-12): The Self is separate from the environment and your body. The Self is ever-lasting. So is the Lord.",
+    "7 (2-12): Is there anything other than the observable universe"];
+    verseNum = randomFeed(numOfVerses, numOfVersesDisplayed)
+    var i = 0;
+    while(i < numOfVersesDisplayed) {
+        var varVerse = document.getElementById("verseFeed" + (i+1));
+        varVerse.innerHTML = verse[verseNum[i]-1];
+        i++;
+    }
+}
